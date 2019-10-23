b4a8d82a2355bc596cde79a620accc0b7c470140
Bram de Leeuw <bram@xd.nl>
2019-10-23 15:22:32 +0200

    complete purchase test

--- src/Message/Request/CompletePurchaseRequest.php
+++ src/Message/Request/CompletePurchaseRequest.php
@@ -9,6 +9,15 @@ use Omnipay\Rabobank\Message\Response\CompletePurchaseResponse;
  */
 class CompletePurchaseRequest extends PurchaseRequest
 {
+    public function getData()
+    {
+        $data = $this->httpRequest->query->all();
+        $data['order_id'] = $this->httpRequest->query->get('order_id');
+        $data['status'] = $this->httpRequest->query->get('status');
+        $data['signature'] = $this->httpRequest->query->get('order_id');
+        return $data;
+    }
+    
     public function sendData($data)
     {
         return $this->response = new CompletePurchaseResponse($this, $data);
--- src/Message/Response/CompletePurchaseResponse.php
+++ src/Message/Response/CompletePurchaseResponse.php
@@ -2,57 +2,15 @@
 
 namespace Omnipay\Rabobank\Message\Response;
 
-use Omnipay\Common\Message\RedirectResponseInterface;
-use Omnipay\Rabobank\Message\Request\AbstractRabobankRequest;
-
-class CompletePurchaseResponse extends PurchaseResponse
+class CompletePurchaseResponse extends AbstractRabobankResponse
 {
-    public function test($method = 'test')
-    {
-        echo "<pre>";
-        print_r($method . PHP_EOL);
-        print_r($this->data);
-        echo "</pre>";
-        exit();
-    }
-
     public function isSuccessful()
     {
-        $this->test('isSuccessful');
-    }
-
-    public function getCode()
-    {
-        $this->test('getCode');
-    }
-
-    public function getMessage()
-    {
-        $this->test('getMessage');
-    }
-
-    public function getStatus()
-    {
-        $this->test('getStatus');
-    }
-
-    public function getTransactionId()
-    {
-        $this->test('getTransactionId');
-    }
-
-    public function getPaymentMethod()
-    {
-        $this->test('getPaymentMethod');
-    }
-
-    public function getAuthorisationId()
-    {
-        $this->test('getAuthorisationId');
+        return isset($this->data['status']) && $this->data['status'] === 'COMPLETED';
     }
 
     public function getOrderId()
     {
-        $this->test('getOrderId');
+        return isset($this->data['order_id']) ? $this->data['order_id'] : null;
     }
 }
