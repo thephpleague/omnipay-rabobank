c668491a0c94255473401fdff296bf8a806c184e
Bram de Leeuw <bram@xd.nl>
2019-10-23 14:03:30 +0200

    complete purchase test

--- src/Message/Response/CompletePurchaseResponse.php
+++ src/Message/Response/CompletePurchaseResponse.php
@@ -7,13 +7,52 @@ use Omnipay\Rabobank\Message\Request\AbstractRabobankRequest;
 
 class CompletePurchaseResponse extends PurchaseResponse
 {
-    public function __construct(AbstractRabobankRequest $request, $data)
+    public function test($method = 'test')
     {
-        parent::__construct($request, $data);
-
         echo "<pre>";
-        print_r($data);
+        print_r($method . PHP_EOL);
+        print_r($this->data);
         echo "</pre>";
         exit();
+    }
+
+    public function isSuccessful()
+    {
+        $this->test('isSuccessful');
+    }
+
+    public function getCode()
+    {
+        $this->test('getCode');
+    }
+
+    public function getMessage()
+    {
+        $this->test('getMessage');
+    }
+
+    public function getStatus()
+    {
+        $this->test('getStatus');
+    }
+
+    public function getTransactionId()
+    {
+        $this->test('getTransactionId');
+    }
+
+    public function getPaymentMethod()
+    {
+        $this->test('getPaymentMethod');
+    }
+
+    public function getAuthorisationId()
+    {
+        $this->test('getAuthorisationId');
+    }
+
+    public function getOrderId()
+    {
+        $this->test('getOrderId');
     }
 }
